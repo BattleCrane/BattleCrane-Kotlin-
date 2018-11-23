8c7cdce8747a6c0bdc3463e62b1cad7d1d57e93e
Igor <ardar.igor@mail.ru>
2018-11-23 16:59:39 +0300

    Repaired battle field

--- app/src/main/java/com/orego/battlecrane/repository/game/manager/field/BBattleField.java
+++ app/src/main/java/com/orego/battlecrane/repository/game/manager/field/BBattleField.java
@@ -5,21 +5,21 @@ import com.orego.battlecrane.repository.game.model.unit.field.BField;
 
 public final class BBattleField {
     
-    private static final int CELL_SIDE = 16;
+    public static final int FIELD_SIDE = 16;
 
-    private static final int CELL_RANGE = CELL_SIDE * CELL_SIDE;
+    private static final int FIELD_RANGE = FIELD_SIDE * FIELD_SIDE;
     
-    private final BUnit[][] field = new BUnit[CELL_SIDE][CELL_SIDE];
+    private final BUnit[][] field = new BUnit[FIELD_SIDE][FIELD_SIDE];
 
     public BBattleField() {
-        for (int i = 0; i < CELL_SIDE; i++) {
-            for (int j = 0; j < CELL_SIDE; j++) {
+        for (int i = 0; i < FIELD_SIDE; i++) {
+            for (int j = 0; j < FIELD_SIDE; j++) {
                 this.field[i][j] = new BField();
             }
         }
     }
     
     public final int getCellCount() {
-        return CELL_RANGE;
+        return FIELD_RANGE;
     }
 }
--- app/src/main/java/com/orego/battlecrane/ui/fragment/battle/BBattleFragment.kt
+++ app/src/main/java/com/orego/battlecrane/ui/fragment/battle/BBattleFragment.kt
@@ -8,52 +8,43 @@ import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.orego.battlecrane.R
+import com.orego.battlecrane.repository.game.manager.field.BBattleField.FIELD_SIDE
 import com.orego.battlecrane.ui.fragment.BFragment
 import kotlinx.android.synthetic.main.fragment_battle.*
 
-const val A = 52
-
 class BBattleFragment : BFragment() {
-
 
     override fun onCreateContentView(i: LayoutInflater, c: ViewGroup?, b: Bundle?): View? = i
         .inflate(R.layout.fragment_battle, c, false)!!
 
     override fun onContentViewCreated() {
-        val idList = mutableListOf<Int>()
+        this.field_constraint_layout.viewTreeObserver.addOnGlobalLayoutListener { this.renderCells() }
+    }
 
-        for (i in 0 until 16) {
+    private fun renderCells() {
+        val measuredCellSide = this.field_constraint_layout.measuredWidth / FIELD_SIDE
+        val idCellList = mutableListOf<Int>()
+        for (i in 0 until FIELD_SIDE) {
             val imageView = ImageView(this.context)
             imageView.id = View.generateViewId()
             imageView.setImageDrawable(this.context!!.getDrawable(R.drawable.ic_action_name))
-            imageView.layoutParams = ConstraintLayout.LayoutParams(32, 32)
+            imageView.layoutParams = ConstraintLayout.LayoutParams(measuredCellSide, measuredCellSide)
             this.field_constraint_layout.addView(imageView)
-            idList.add(imageView.id)
+            idCellList.add(imageView.id)
         }
         val constraintSet = ConstraintSet()
         constraintSet.clone(this.field_constraint_layout)
-        for (i in 0 until 16) {
+        for (i in 0 until FIELD_SIDE) {
+            val cellId = idCellList[i]
+            val x = i * measuredCellSide
             constraintSet.connect(
-                idList[i],
+                cellId,
                 ConstraintSet.LEFT,
                 this.field_constraint_layout.id,
                 ConstraintSet.LEFT,
-                i * 32
+                x
             )
         }
         constraintSet.applyTo(this.field_constraint_layout)
-
     }
 }
-
-//val params = ConstraintLayout.LayoutParams(32, 32)
-//val imageView = ImageView(this.context)
-//imageView.id = View.generateViewId()
-//imageView.setImageDrawable(this.context!!.getDrawable(R.drawable.ic_action_name))
-//imageView.layoutParams = params
-//this.field_constraint_layout.addView(imageView)
-//
-//val constraintSet = ConstraintSet()
-//constraintSet.clone(this.field_constraint_layout)
-//constraintSet.connect(imageView.id, ConstraintSet.LEFT, this.field_constraint_layout.id, ConstraintSet.LEFT, 32)
-//constraintSet.applyTo(this.field_constraint_layout)
