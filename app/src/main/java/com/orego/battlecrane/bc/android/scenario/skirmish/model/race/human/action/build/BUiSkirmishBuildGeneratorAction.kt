39c7b579d617ca7b6c8010c9486eeadd5226d04a
StarKRE <ardar.igor@mail.ru>
2019-03-06 13:05:26 +0300

    Created  ui actions

---
+++ app/src/main/java/com/orego/battlecrane/bc/android/scenario/skirmish/model/race/human/action/build/BUiSkirmishBuildGeneratorAction.kt
@@ -0,0 +1,91 @@
+package com.orego.battlecrane.bc.android.scenario.skirmish.model.race.human.action.build
+
+import com.orego.battlecrane.bc.android.api.context.BUiGameContext
+import com.orego.battlecrane.bc.android.api.context.clickController.BUiClickMode
+import com.orego.battlecrane.bc.android.api.model.action.BUiAction
+import com.orego.battlecrane.bc.android.api.model.unit.BUiUnit
+import com.orego.battlecrane.bc.android.standardImpl.race.human.asset.BUiHumanAssets
+import com.orego.battlecrane.bc.engine.api.context.BGameContext
+import com.orego.battlecrane.bc.engine.api.model.unit.type.BEmptyField
+import com.orego.battlecrane.bc.engine.api.model.util.BProducable
+import com.orego.battlecrane.bc.engine.scenario.skirmish.model.race.human.event.construct.BSkirmishHumanConstructGeneratorEvent
+import com.orego.battlecrane.bc.engine.scenario.skirmish.model.race.human.utils.BSkirmishHumanRule
+import com.orego.battlecrane.bc.engine.standardImpl.race.human.util.BHumanCalculations
+
+class BUiSkirmishBuildGeneratorAction(uiGameContext: BUiGameContext, private val producable: BProducable) :
+    BUiAction(uiGameContext) {
+
+    companion object {
+
+        const val PATH = "${BUiHumanAssets.Action.Build.PATH}/barracks"
+    }
+
+    override val uiClickMode by lazy {
+        UiClickMode(uiGameContext)
+    }
+
+    /**
+     * Update.
+     */
+
+    override fun createPath(): String {
+        val viewKey = this.viewMode.key
+        return "$PATH/$viewKey.png"
+    }
+
+    /**
+     * Active.
+     */
+
+    override fun canActivate(uiGameContext: BUiGameContext): Boolean {
+        val context = uiGameContext.gameContext
+        val playerId = this.producable.playerId
+        val generatorCount = BHumanCalculations.countGenerators(context, playerId)
+        return this.producable.isProduceEnable && generatorCount < BSkirmishHumanRule.GENERATOR_LIMIT
+    }
+
+    /**
+     * Select.
+     */
+
+    override fun onSelect(uiGameContext: BUiGameContext) {
+    }
+
+    override fun showDescription(uiGameContext: BUiGameContext) {
+    }
+
+    /**
+     * Dismiss.
+     */
+
+    override fun onPerform(uiGameContext: BUiGameContext) {
+        this.dismiss(uiGameContext)
+    }
+
+    inner class UiClickMode(uiGameContext: BUiGameContext) : BUiAction.UiClickMode(uiGameContext, this) {
+
+        private val producable = this@BUiSkirmishBuildGeneratorAction.producable
+
+        private val gameContext: BGameContext = uiGameContext.gameContext
+
+        override fun onNextClickMode(nextUiClickMode: BUiClickMode?): BUiClickMode? {
+            if (nextUiClickMode is BUiUnit.UiClickMode) {
+                val clickedUnit = nextUiClickMode.uiUnit.unit
+                if (clickedUnit is BEmptyField) {
+                    val playerId = this.producable.playerId
+                    val producableId = this.producable.producableId
+                    val x = clickedUnit.x
+                    val y = clickedUnit.y
+                    val event = BSkirmishHumanConstructGeneratorEvent(producableId, x, y)
+                    val isSuccessful = event.isEnable(this.gameContext, playerId)
+                    if (isSuccessful) {
+                        this.gameContext.pipeline.broacastEvent(event)
+                        this.action.onPerform(this.uiGameContext)
+                        return null
+                    }
+                }
+            }
+            return this
+        }
+    }
+}
